607e55f8fd1d8ff7d32acddfc71c7b161bb66559
Farid Hernandez <farid.hdz.64@gmail.com>
2021-09-02 09:03:53 -0600

    fix: Removing native and web configurations

--- src/eslint-config/index.ts
+++ src/eslint-config/index.ts
@@ -1,11 +1,4 @@
 import eslintBaseConfig = require('./base');
 import eslintTSBaseConfig = require('./ts-base');
-import eslintWebConfig = require('./web');
-import eslintNativeConfig = require('./native');
 
-export {
-  eslintBaseConfig,
-  eslintTSBaseConfig,
-  eslintWebConfig,
-  eslintNativeConfig,
-};
+export { eslintBaseConfig, eslintTSBaseConfig };
